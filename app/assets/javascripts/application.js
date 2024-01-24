7c1ce82201f3c92aeed9d42d79d481784fb39131
Denis Davidov <denislav.davidov@digital.justice.gov.uk>
2024-01-24 11:17:42 +0000

    updating prototype

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,34 @@
-/* global $ */
+//
+// For guidance on how to add JavaScript see:
+// https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
+//
 
-// Warn about using the kit in production
-if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
+window.GOVUKPrototypeKit.documentReady(() => {
+new MOJFrontend.FilterToggleButton({
+  bigModeMediaQuery: "(min-width: 48.063em)",
+  startHidden: true,
+  toggleButton: {
+    container: $(".moj-action-bar__filter"),
+    showText: "Show filter",
+    hideText: "Hide filter",
+    classes: "govuk-button--secondary",
+  },
+  closeButton: {
+    container: $(".moj-filter__header-action"),
+    text: "Close",
+  },
+  filter: {
+    container: $(".moj-filter"),
+  },
+});
+
+if (typeof MOJFrontend.MultiFileUpload !== "undefined") {
+  new MOJFrontend.MultiFileUpload({
+    container: $(".moj-multi-file-upload"),
+    uploadUrl: "/ajax-upload",
+    deleteUrl: "/ajax-delete",
+  });
 }
-
-$(document).ready(function () {
-  window.GOVUKFrontend.initAll()
 })
+
+

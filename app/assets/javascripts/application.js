fd1f56457034f9510b1b9a1c27192f1ec5759405
Andrew Duckworth - MOJ <andrew.duckworth@digital.justice.gov.uk>
2024-07-03 16:03:28 +0100

    update goal pages to work as expected

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,31 +4,38 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-new MOJFrontend.FilterToggleButton({
-  bigModeMediaQuery: "(min-width: 48.063em)",
-  startHidden: true,
-  toggleButton: {
-    container: $(".moj-action-bar__filter"),
-    showText: "Show filter",
-    hideText: "Hide filter",
-    classes: "govuk-button--secondary",
-  },
-  closeButton: {
-    container: $(".moj-filter__header-action"),
-    text: "Close",
-  },
-  filter: {
-    container: $(".moj-filter"),
-  },
-});
-
-if (typeof MOJFrontend.MultiFileUpload !== "undefined") {
-  new MOJFrontend.MultiFileUpload({
-    container: $(".moj-multi-file-upload"),
-    uploadUrl: "/ajax-upload",
-    deleteUrl: "/ajax-delete",
+  new MOJFrontend.FilterToggleButton({
+    bigModeMediaQuery: "(min-width: 48.063em)",
+    startHidden: true,
+    toggleButton: {
+      container: $(".moj-action-bar__filter"),
+      showText: "Show filter",
+      hideText: "Hide filter",
+      classes: "govuk-button--secondary",
+    },
+    closeButton: {
+      container: $(".moj-filter__header-action"),
+      text: "Close",
+    },
+    filter: {
+      container: $(".moj-filter"),
+    },
   });
-}
+
+
+  const goal = [
+    "I will find suitable accommodation that's safe and secure",
+    'I will keep my current accommodation'
+  ]
+
+  accessibleAutocomplete({
+    element: document.querySelector('#createGoalAcc'),
+    id: 'createGoalAcc', // To match it to the existing <label>.
+    name: 'createGoalAcc',
+    source: goal,
+    displayMenu: 'overlay'
+  })
+
 })
 
 

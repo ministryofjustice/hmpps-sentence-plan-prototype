42dd16e6319c9084aeef77c64116898291d68e59
Andrew Duckworth - MOJ <andrew.duckworth@digital.justice.gov.uk>
2024-07-03 20:06:46 +0100

    Pull autocomplete out of goal pages to stop error on other pages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,20 +22,6 @@ window.GOVUKPrototypeKit.documentReady(() => {
     },
   });
 
-
-  const goal = [
-    "I will find suitable accommodation that's safe and secure",
-    'I will keep my current accommodation'
-  ]
-
-  accessibleAutocomplete({
-    element: document.querySelector('#goalObjective'),
-    id: 'goalObjective', // To match it to the existing <label>.
-    name: 'goalObjective',
-    source: goal,
-    displayMenu: 'overlay'
-  })
-
 })
 
 
---
+++ app/assets/javascripts/goals-accommodation.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    const goal = [
+        "I will find suitable accommodation that's safe and secure",
+        'I will keep my current accommodation'
+      ]
+    
+      accessibleAutocomplete({
+        element: document.querySelector('#goalObjective'),
+        id: 'goalObjective', // To match it to the existing <label>.
+        name: 'goalObjective',
+        source: goal,
+        displayMenu: 'overlay'
+      })
+  };

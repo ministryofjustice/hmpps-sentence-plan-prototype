93eef733a5932646de4ca385fa634a6fb0f09be3
Phoebe <phoebe.brocket@digital.justice.gov.uk>
2024-07-05 16:08:18 +0100

    updating agreed plan view

--- app/routes.js
+++ app/routes.js
@@ -241,3 +241,23 @@ router.get(`/${DESIGN_VERSION}/plan-overview-later`, (req, res, next) => {
         GOALS_FOR_LATER_DATA: req.session.data.goals.filter(goal => !goal.date)
     })
 })
+
+router.get(`/${DESIGN_VERSION}/agreed-plan`, (req, res, next) => {
+    /**
+     *  We can now access all goal data through {{ GOALS_DATA }} in our HTML/template
+     */
+    return res.render(`${DESIGN_VERSION}/agreed-plan.html`, {
+        GOALS_DATA: req.session.data.goals.filter(goal => goal.date),
+        GOALS_FOR_LATER_DATA: req.session.data.goals.filter(goal => !goal.date)
+    })
+})
+
+router.get(`/${DESIGN_VERSION}/agreed-plan-later`, (req, res, next) => {
+    /**
+     *  We can now access all goal data through {{ GOALS_DATA }} in our HTML/template
+     */
+    return res.render(`${DESIGN_VERSION}/agreed-plan-later.html`, {
+        GOALS_DATA: req.session.data.goals.filter(goal => goal.date),
+        GOALS_FOR_LATER_DATA: req.session.data.goals.filter(goal => !goal.date)
+    })
+})

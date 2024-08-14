673c29a14647b88308bf7af662c92f8a27f4bcbf
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-08-14 21:29:33 +0100

    Added plan-overview-removed

--- app/routes.js
+++ app/routes.js
@@ -320,6 +320,15 @@ router.get(`/${DESIGN_VERSION}/plan-overview`, (req, res, next) => {
     })
 })
 
+router.get(`/${DESIGN_VERSION}/plan-overview-removed`, (req, res, next) => {
+    return res.render(`${DESIGN_VERSION}/plan-overview-removed.html`, {
+        ACTIVE_GOALS: req.session.data.goals.filter(goal => goal.status === 'ACTIVE'),
+        FUTURE_GOALS: req.session.data.goals.filter(goal => goal.status === 'FUTURE'),
+        REMOVED_GOALS: req.session.data.goals.filter(goal => goal.status === 'REMOVED'),
+        ACHIEVED_GOALS: req.session.data.goals.filter(goal => goal.status === 'ACHIEVED')
+    })
+})
+
 router.get(`/${DESIGN_VERSION}/plan-overview-later`, (req, res, next) => {
     /**
      *  We can now access all goal data through {{ GOALS_DATA }} in our HTML/template

6a65b154d6a81779a49c88f6951dd318cf18a816
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-08-14 22:53:42 +0100

    Add a pretty print date filter ✨

--- app/filters.js
+++ app/filters.js
@@ -1,2 +1,15 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
+
+addFilter('prettyPrintDate', (date) => {
+    return new Date(date)
+        .toLocaleString('en-GB', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+        })
+        .replace(',', ' at');
+})
